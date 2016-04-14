4e0eba511060a5aaf1acb4070ba1a65ef9219225
lucasnogueira955 <lucasnogueira955@outlook.com>
2016-04-14 00:15:29 -0300

    clase circulo

--- tarea1/src/ar/edu/unlam/tallerweb/tarea1/Circulo.java
+++ tarea1/src/ar/edu/unlam/tallerweb/tarea1/Circulo.java
@@ -1,5 +1,15 @@
 package ar.edu.unlam.tallerweb.tarea1;
 
 public class Circulo {
-
+	public Float radio = (float)6;
+	public Float pi = (float)Math.PI;
+	
+	public float perimetro(){
+		return (2 * pi * radio);
+	}
+	
+	public float area(){
+		return( pi * (radio*radio));
+	}
+	
 }
--- tarea1/test/ar/edu/unlam/tallerweb/tarea1/CuadradoTest.java
+++ tarea1/test/ar/edu/unlam/tallerweb/tarea1/CuadradoTest.java
@@ -1,14 +1,29 @@
 package ar.edu.unlam.tallerweb.tarea1;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CuadradoTest {
 
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TestPerimetroCuadrados(){
+		Cuadrado miCuadrado = new Cuadrado();
+		Float valorEsperado = (float)16;
+		
+		Integer valorObtenido = miCuadrado.perimetro();
+		Assert.assertEquals(valorEsperado, valorObtenido);
+		miCuadrado = null;
+	}		
+	
+	@Test
+	public void TestAreaCuadrados(){
+		Cuadrado miCuadrado = new Cuadrado();
+		Float valorEsperado = (Float)16.0;
+		
+		Double valorObtenido = miCuadrado.area();
+		Assert.assertEquals(valorEsperado, valorObtenido);
+		miCuadrado = null;
 	}
 
 }
